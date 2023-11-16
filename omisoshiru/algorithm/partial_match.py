70a55ddabe3a226ff6d53665d65b21d1cc341586
S. Shibasaki <s-shibasaki@sakura-is.co.jp>
2023-11-16 12:07:52 +0900

    Add partial_match_multi function to algorithm module
    - Updated omisoshiru.algorithm.__init__.py to import partial_match_multi function.
    - Added partial_match_multi function to omisoshiru.algorithm.partial_match.py for performing partial matches on multiple patterns.
    - Created test_partial_match.py to include unit tests for the partial_match and partial_match_multi functions.
    - Implemented test cases to verify the functionality of the partial_match_multi function in different scenarios.

--- omisoshiru/algorithm/partial_match.py
+++ omisoshiru/algorithm/partial_match.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 def partial_match(pat: list, tgt: list):
     matches = []
 
@@ -6,6 +9,25 @@ def partial_match(pat: list, tgt: list):
         if tgt[i : i + len(pat)] == pat:
             matches.append(i)
             i += len(pat)
+        else:
+            i += 1
+
+    return matches
+
+
+def partial_match_multi(patterns: List[list], tgt: list):
+    # 長さの降順にソート
+    patterns = sorted(patterns, key=len, reverse=True)
+
+    matches = []
+
+    i = 0
+    while i < len(tgt):
+        for pat in patterns:
+            if tgt[i : i + len(pat)] == pat:
+                matches.append((i, pat))
+                i += len(pat)
+                break
         else:
             i += 1
 
--- tests/algorithm/test_partial_match.py
+++ tests/algorithm/test_partial_match.py
@@ -1,4 +1,4 @@
-from omisoshiru.algorithm import partial_match
+from omisoshiru.algorithm import partial_match, partial_match_multi
 
 
 def test_single_match():
@@ -20,3 +20,17 @@ def test_overlapped_matches():
     tgt = list("xxxababaxxx")
     excepted_result = [3]
     assert partial_match(pat, tgt) == excepted_result
+
+
+def test_multiple_patterns_single_match():
+    patterns = [list("cde"), list("cdefg")]
+    tgt = list("abcdefg")
+    excepted_result = [(2, list("cdefg"))]
+    assert partial_match_multi(patterns, tgt) == excepted_result
+
+
+def test_multiple_patterns_multiple_matches():
+    patterns = [list("cde"), list("cdefg")]
+    tgt = list("abcdefgabcde")
+    excepted_result = [(2, list("cdefg")), (9, list("cde"))]
+    assert partial_match_multi(patterns, tgt) == excepted_result
