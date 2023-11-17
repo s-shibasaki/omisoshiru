7031f72996f278abeddad7498a1cff16e60ebc86
S. Shibasaki <s-shibasaki@sakura-is.co.jp>
2023-11-17 12:56:23 +0900

    Add Wakachi class and its tests - Added `Wakachi` class to perform word tokenization using MeCab. - Added test cases for `Wakachi` class to validate the word tokenization functionality. - Updated the import statements in `omisoshiru.text.__init__.py` file to include `Wakachi` class. - Updated `WakachiMatcher` class to use `Wakachi` class for word tokenization and updated its tests accordingly.

---
+++ tests/text/test_wakachi.py
@@ -0,0 +1,33 @@
+import pytest
+
+from omisoshiru.text import Wakachi
+
+
+@pytest.fixture
+def wakachi():
+    return Wakachi()
+
+
+@pytest.fixture
+def wakachi_preserve_space():
+    return Wakachi(preserve_space=True)
+
+
+# 正常系のテストケース
+def test_parse_no_space(wakachi):
+    string = "桜が咲く季節です"
+    expected_result = ["桜", "が", "咲く", "季節", "です"]
+    assert wakachi.parse(string) == expected_result
+
+
+def test_parse_with_space_preserve(wakachi_preserve_space):
+    string = "桜が咲く季 節です"
+    expected_result = ["桜", "が", "咲く", "季", "節", "です"]
+    assert wakachi_preserve_space.parse(string) == expected_result
+
+
+# エラーのテストケース
+def test_parse_with_space_error(wakachi):
+    string = "桜が咲く季 節です"
+    with pytest.raises(ValueError):
+        wakachi.parse(string)
