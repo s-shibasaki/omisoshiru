3b894cd238d81da6affb05afe43d45939a5b7919
S. Shibasaki <s-shibasaki@sakura-is.co.jp>
2023-11-22 03:47:18 +0900

    update multi_sentence_sum.py

--- omisoshiru/ml/models/multi_sentence_sum.py
+++ omisoshiru/ml/models/multi_sentence_sum.py
@@ -10,11 +10,26 @@ class MultiSentenceSum(nn.Module):
         super().__init__()
         self._model = AutoModel.from_pretrained(pretrained_model_name)
         self._tokenizer = AutoTokenizer.from_pretrained(pretrained_model_name)
+        self._tokenization_options = {
+            "return_tensors": "pt",
+            "padding": True,
+            "truncation": True,
+        }
 
-    def forward(self, sentences_batch: List[List[str]]):
-        last_hidden_states = []
-        for sentence_batch in sentences_batch:
-            inputs = self._tokenizer(sentence_batch)
-            outputs = self._model(**inputs)
-            last_hidden_states.append(outputs.last_hidden_states)
-        return torch.stack(last_hidden_states).sum(dim=0)
+    def forward(self, sentences: List[List[str]], weights: torch.Tensor):
+        """_summary_
+
+        Args:
+            sentences (List[List[str]]): shape: (num_sentences, num_batches)
+            weights (torch.Tensor): shape: (num_sentences, num_batches)
+
+        Returns:
+            _type_: _description_
+        """
+        output_pool = []
+        for sentence in sentences:
+            inputs = self._tokenizer(sentence, **self._tokenization_options)
+            output_pool.append(self._model(**inputs).pooler_output)
+        print(output_pool[0].size())
+        weighted_outputs = torch.stack(output_pool) * weights.unsqueeze(-1)
+        return weighted_outputs
