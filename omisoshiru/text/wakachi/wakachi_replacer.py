9becfb79164e2e4786674c4dd8659dc876f3d25c
S. Shibasaki <sdeb6vm@gmail.com>
2023-11-22 14:52:36 +0900

    feat: Enhance WakachiReplacer initialization options
    Improve the initialization of WakachiReplacer by adding optional bool parameters unify_hz and
    unify_hl. These parameters determine whether to unify half-width characters and characters to
    lowercase, respectively. The default values are set to None, and if not provided, the class
    initializes them to None. This enhancement allows for more customization when creating instances
    of WakachiReplacer.

--- omisoshiru/text/wakachi/wakachi_replacer.py
+++ omisoshiru/text/wakachi/wakachi_replacer.py
@@ -1,17 +1,24 @@
-from typing import Dict
+from typing import Dict, Optional
 
 from ..replace_text_ranges import replace_text_ranges
 from .wakachi_matcher import WakachiMatcher
 
 
 class WakachiReplacer:
-    def __init__(self, replace_dict: Dict[str, str], unify_hz=False, unify_hl=False):
+    def __init__(
+        self,
+        replace_dict: Dict[str, str],
+        unify_hz: Optional[bool] = None,
+        unify_hl: Optional[bool] = None,
+    ):
         """
         A WakachiReplacer performs replacements in a given text based on a predefined dictionary.
 
         Args:
             replace_dict (Dict[str, str]): A dictionary where keys are patterns to be replaced,
                 and values are the replacement strings.
+            unify_hz (bool, optional): Whether to unify half-width characters. Defaults to None.
+            unify_hl (bool, optional): Whether to unify characters to lowercase. Defaults to None.
 
         Example:
             >>> replacer = WakachiReplacer({"りんご": "フルーツ", "ばなな": "フルーツ"})
@@ -39,7 +46,7 @@ class WakachiReplacer:
         )
         return text
 
-    def __str__(self):
+    def __str__(self) -> str:
         """
         Returns a string representation of the WakachiReplacer instance.
 
