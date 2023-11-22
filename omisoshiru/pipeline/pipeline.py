2cf91477c037f43881f5d0bebc024fb52f982bc7
S. Shibasaki <sdeb6vm@gmail.com>
2023-11-23 00:43:46 +0900

    chore: Update pipeline module
    - Refactor get_pipeline_dir to include type hints
    - Improve documentation for functions and methods in pipeline module
    - Add type hints to function parameters and return types

--- omisoshiru/pipeline/pipeline.py
+++ omisoshiru/pipeline/pipeline.py
@@ -5,7 +5,13 @@ import nbformat
 from nbconvert.preprocessors import ExecutePreprocessor
 
 
-def get_pipeline_dir():
+def get_pipeline_dir() -> str:
+    """
+    Get the pipeline directory path.
+
+    Returns:
+        str: The path to the pipeline directory.
+    """
     return os.environ.get("PIPELINE_DIR") or os.path.abspath(os.getcwd())
 
 
@@ -16,21 +22,35 @@ class Run:
         node: Any,
         inputs: dict,
         params: dict,
-        kernel_name="",
-        timeout=None,
+        kernel_name: str = "",
+        timeout: Optional[int] = None,
     ):
+        """
+        Initialize a Run object.
+
+        Parameters:
+            name (str): The name of the run.
+            node (Any): The associated Node object.
+            inputs (dict): Input data for the run.
+            params (dict): Parameters for the run.
+            kernel_name (str, optional): The kernel name for notebook execution. Default is "".
+            timeout (Optional[int], optional): The timeout for notebook execution. Default is None.
+        """
         self.name = name
         self.node = node
         self.inputs = inputs
         self.params = params
-
         self.run(kernel_name, timeout)
 
-    def run(self, kernel_name, timeout):
-        os.makedirs(self.get_dir(), exist_ok=True)
+    def run(self, kernel_name: str, timeout: Optional[int]):
+        """
+        Run the associated Node with the specified parameters.
 
-        # for src, dst in zip(self.inputs, self.node.inputs):
-        #     os.symlink(os.path.abspath(src), os.path.join(self.name, dst))
+        Parameters:
+            kernel_name (str): The kernel name for notebook execution.
+            timeout (Optional[int]): The timeout for notebook execution.
+        """
+        os.makedirs(self.get_dir(), exist_ok=True)
 
         os.environ.update(
             **{
@@ -51,7 +71,13 @@ class Run:
         ) as f:
             nbformat.write(nb, f)
 
-    def get_dir(self):
+    def get_dir(self) -> str:
+        """
+        Get the directory path for the run.
+
+        Returns:
+            str: The path to the run directory.
+        """
         return os.path.join(get_pipeline_dir(), "runs", self.name)
 
 
@@ -63,6 +89,15 @@ class Node:
         outputs: List[str],
         params: List[str],
     ):
+        """
+        Initialize a Node object.
+
+        Parameters:
+            name (str): The name of the node.
+            inputs (List[str]): Input names for the node.
+            outputs (List[str]): Output names for the node.
+            params (List[str]): Parameter names for the node.
+        """
         self.name = name
         self.inputs = inputs
         self.outputs = outputs
@@ -76,7 +111,20 @@ class Node:
         params: Dict[str, str],
         timeout: Optional[int] = None,
         kernel_name: Optional[str] = "",
-    ):
+    ) -> Run:
+        """
+        Run the node with the specified parameters.
+
+        Parameters:
+            name (str): The name of the run.
+            inputs (Dict[str, Tuple[Run, str]]): Input data for the run.
+            params (Dict[str, str]): Parameters for the run.
+            timeout (Optional[int], optional): The timeout for notebook execution. Default is None.
+            kernel_name (Optional[str], optional): The kernel name for notebook execution. Default is "".
+
+        Returns:
+            Run: The Run object associated with the run.
+        """
         run = Run(
             name=name,
             node=self,
@@ -88,7 +136,13 @@ class Node:
         self.runs.append(run)
         return run
 
-    def get_file(self):
+    def get_file(self) -> str:
+        """
+        Get the file path for the node.
+
+        Returns:
+            str: The path to the node file.
+        """
         return os.path.join(
             get_pipeline_dir(), "nodes", self.name, self.name + ".ipynb"
         )
@@ -96,10 +150,25 @@ class Node:
 
 class Pipeline:
     def __init__(self):
+        """
+        Initialize a Pipeline object.
+        """
         self.nodes = []
 
     def add_node(self, node: Node):
+        """
+        Add a Node to the pipeline.
+
+        Parameters:
+            node (Node): The Node object to add to the pipeline.
+        """
         self.nodes.append(node)
 
-    def get_runs(self):
+    def get_runs(self) -> List[Run]:
+        """
+        Get a list of all runs in the pipeline.
+
+        Returns:
+            List[Run]: A list of Run objects in the pipeline.
+        """
         return [run for node in self.nodes for run in node.runs]
