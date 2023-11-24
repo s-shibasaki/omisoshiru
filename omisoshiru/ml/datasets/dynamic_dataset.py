14473b01725c77c521363b39c1badd1bb5306b70
Shungo Shibasaki <s-shibasaki@sakura-is.co.jp>
2023-11-25 08:21:05 +0900

    refacter dynamic_dataset.py

--- omisoshiru/ml/datasets/dynamic_dataset.py
+++ omisoshiru/ml/datasets/dynamic_dataset.py
@@ -2,59 +2,23 @@ from torch.utils.data import IterableDataset
 
 
 class DynamicDataset(IterableDataset):
-    """
-    A PyTorch IterableDataset implementation for handling dynamic and iterable datasets.
-
-    This dataset allows for dynamic initialization through the provided `initialize_function`, enabling flexibility
-    in data loading strategies. It is designed to be used with PyTorch's DataLoader for efficient and flexible
-    data loading during training or evaluation.
-
-    Parameters:
-    - data (iterable): The raw data that the dataset will iterate over.
-    - initialize_function (callable, optional): A function used for dynamic initialization of the dataset. If None,
-      the dataset will default to a simple identity initialization function. The function should take the raw data
-      as input and return the data to be used for the current epoch.
-
-    Example:
-    ```python
-    # Example usage with DataLoader
-    data = [1, 2, 3, 4, 5]
-    initialize_function = lambda x: x[::-1]  # Reverses the data for each epoch
-    dynamic_dataset = DynamicDataset(data, initialize_function)
-
-    dataloader = DataLoader(dynamic_dataset, batch_size=2, num_workers=2)
-    for batch in dataloader:
-        # Training or evaluation logic with the batch
-    ```
-
-    Note:
-    - The `initialize_function` should be a callable that takes the raw data and returns the data for the current epoch.
-      It allows for dynamic changes to the dataset between epochs.
-    - This dataset is intended to be used with PyTorch's DataLoader, which handles the iteration and parallelization
-      aspects efficiently.
-
-    References:
-    - PyTorch IterableDataset: https://pytorch.org/docs/stable/data.html#torch.utils.data.IterableDataset
-    """
-
-    def __init__(self, data, on_epoch_start=None):
-        if on_epoch_start is None:
-            on_epoch_start = lambda x: x
-        elif not callable(on_epoch_start):
-            raise TypeError("on_epoch_start must be callable")
+    def __init__(self, data, epoch_processor=None):
+        if epoch_processor is None:
+            epoch_processor = lambda x: x
+        elif not callable(epoch_processor):
+            raise TypeError("epoch_processor must be callable")
 
         self.data = data
-        self.on_epoch_start = on_epoch_start
+        self.epoch_processor = epoch_processor
         self.epoch = 0
 
     def __iter__(self):
-        epoch_data = self.on_epoch_start(self.epoch, self.data)
-        self.epoch_iterator = iter(epoch_data)
+        self.epoch_iterable = self.epoch_processor(self.epoch, self.data)
         return self
 
     def __next__(self):
         try:
-            return next(self.epoch_iterator)
+            return next(self.epoch_iterable)
         except StopIteration:
             self.epoch += 1
             raise StopIteration
