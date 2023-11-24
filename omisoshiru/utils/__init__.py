d2ad902cee746d41ad720f1a889b8ff982d5fc8b
Shungo Shibasaki <s-shibasaki@sakura-is.co.jp>
2023-11-25 08:25:32 +0900

    update utils/__init__.py

--- omisoshiru/utils/__init__.py
+++ omisoshiru/utils/__init__.py
@@ -1,3 +1,6 @@
+from .convert_dict_of_lists_to_list_of_dicts import (
+    convert_dict_of_lists_to_list_of_dicts,
+)
 from .process_batches import process_batches
 
-__all__ = ["process_batches"]
+__all__ = ["process_batches", "convert_dict_of_lists_to_list_of_dicts"]
