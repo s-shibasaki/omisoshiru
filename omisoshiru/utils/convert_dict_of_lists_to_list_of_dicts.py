afac8f3d2f872618fcd86289d3466fe3f91b0bdb
Shungo Shibasaki <s-shibasaki@sakura-is.co.jp>
2023-11-25 09:22:01 +0900

    update convert function

--- omisoshiru/utils/convert_dict_of_lists_to_list_of_dicts.py
+++ omisoshiru/utils/convert_dict_of_lists_to_list_of_dicts.py
@@ -1,17 +1,10 @@
-def convert_dict_of_lists_to_list_of_dicts(dict_of_lists):
-    """
-    Convert a dictionary of lists to a list of dictionaries with the same keys.
+def convert_dict_of_lists_to_list_of_dicts(dict_of_lists, nest=None):
+    if isinstance(nest, int) and nest > 1:
+        dict_of_lists = {
+            k: convert_dict_of_lists_to_list_of_dicts(v, nest - 1)
+            for k, v in dict_of_lists.items()
+        }
 
-    Parameters
-    ----------
-    dict_of_lists : dict
-        A dictionary where the values are lists.
-
-    Returns
-    -------
-    list of dict
-        A list of dictionaries with the same keys as the input dictionary, where the values are taken from the corresponding list in the input dictionary.
-    """
     keys, values = zip(*dict_of_lists.items())
     list_of_dicts = [{k: v for k, v in zip(keys, item)} for item in zip(*values)]
     return list_of_dicts
--- omisoshiru/utils/convert_list_of_dicts_to_dict_of_lists.py
+++ omisoshiru/utils/convert_list_of_dicts_to_dict_of_lists.py
@@ -1,4 +1,23 @@
-def convert_list_of_dicts_to_dict_of_lists(list_of_dicts):
+from typing import Literal, Optional
+
+import numpy as np
+import torch
+
+
+def convert_list_of_dicts_to_dict_of_lists(
+    list_of_dicts, nest=None, stack: Optional[Literal["numpy", "torch"]] = None
+):
     keys, values = zip(*[zip(*i.items()) for i in list_of_dicts])
     dict_of_lists = {k: v for k, v in zip(keys[0], zip(*values))}
+
+    if isinstance(nest, int) and nest > 1:
+        dict_of_lists = {
+            k: convert_list_of_dicts_to_dict_of_lists(v, nest - 1)
+            for k, v in dict_of_lists.items()
+        }
+    elif stack == "numpy":
+        dict_of_lists = {k: np.stack(v) for k, v in dict_of_lists.items()}
+    elif stack == "torch":
+        dict_of_lists = {k: torch.stack(v) for k, v in dict_of_lists.items()}
+
     return dict_of_lists
